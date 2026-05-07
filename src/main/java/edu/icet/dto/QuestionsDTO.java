30bcc6347597f61b50323efbd42429afabb6dfe0
hashini0123 <nanayakkarahashini5@gmail.com>
2026-05-07 10:47:22 +0530

    Refactor Questions and QuestionsDTO classes to use Lombok annotations for improved code readability

--- src/main/java/edu/icet/dto/QuestionsDTO.java
+++ src/main/java/edu/icet/dto/QuestionsDTO.java
@@ -1,29 +1,19 @@
 package edu.icet.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import org.w3c.dom.Text;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class QuestionsDTO {
-
     private String id;
-
     private String quizId;
-
     private Text questionText;
-
     private String optionA;
-
     private String optionB;
-
     private String optionC;
-
     private String optionD;
-
 }
--- src/main/java/edu/icet/model/Questions.java
+++ src/main/java/edu/icet/model/Questions.java
@@ -1,32 +1,18 @@
 package edu.icet.model;
+import lombok.*;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity
-@Table(name = "question")
 public class Questions {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long quizId;
-
-    @Column(columnDefinition = "TEXT")
     private String questionText;
-
     private String optionA;
-
     private String optionB;
-
     private String optionC;
-
     private String optionD;
 
 
