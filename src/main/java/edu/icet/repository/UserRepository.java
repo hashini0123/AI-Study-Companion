0a3cce871f0be315686ebdeb9d8c5a77880292c4
hashini0123 <nanayakkarahashini5@gmail.com>
2026-05-18 01:55:03 +0530

    Add updateUser method to UserRepository interface for user updates

--- src/main/java/edu/icet/repository/UserRepository.java
+++ src/main/java/edu/icet/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package edu.icet.repository;
 
 import edu.icet.dto.UserDTO;
+import org.apache.catalina.User;
 
 public interface UserRepository {
     void save(UserDTO userDTO);
+
+    boolean updateUser(UserDTO userDTO);
 }
