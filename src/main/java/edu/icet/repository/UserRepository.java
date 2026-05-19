07a0374f6a7ee6cd17bdbca88c1f5b464c8b848a
hashini0123 <nanayakkarahashini5@gmail.com>
2026-05-19 22:51:15 +0530

    Add getAll method to UserRepository interface for retrieving all users

--- src/main/java/edu/icet/repository/UserRepository.java
+++ src/main/java/edu/icet/repository/UserRepository.java
@@ -3,10 +3,14 @@ package edu.icet.repository;
 import edu.icet.dto.UserDTO;
 import org.apache.catalina.User;
 
+import java.util.List;
+
 public interface UserRepository {
     void save(UserDTO userDTO);
 
     boolean updateUser(UserDTO userDTO);
 
     boolean deleteById(String id);
+
+    List<UserDTO> getAll();
 }
