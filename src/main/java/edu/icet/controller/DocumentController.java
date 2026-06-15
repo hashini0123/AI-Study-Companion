3b850ecc3eb4f3db9ea34bc5d2c0e76eae2aa3a4
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-15 10:40:07 +0530

    Add updateDocument endpoint to DocumentController for document updates

--- src/main/java/edu/icet/controller/DocumentController.java
+++ src/main/java/edu/icet/controller/DocumentController.java
@@ -3,11 +3,9 @@ package edu.icet.controller;
 import edu.icet.dto.DocumentsDTO;
 import edu.icet.service.DocumentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +18,18 @@ public class DocumentController {
     public ResponseEntity<String> saveDocument(@RequestBody DocumentsDTO documentsDTO){
         documentService.saveDocument(documentsDTO);
         return ResponseEntity.ok("Document saved successfully!");
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> updateDocument(@RequestBody DocumentsDTO documentsDTO){
+        boolean isUpdate = documentService.updateDocument(documentsDTO);
+
+        if(isUpdate){
+            return ResponseEntity.ok("Document update successfully!");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Document not found!");
+        }
+
     }
 
 
