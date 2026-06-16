ea51597075cc9edaca630591f81c36285553b12b
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-16 11:41:34 +0530

    Add method to retrieve all documents in DocumentRepository and DocumentRepositoryImpl

--- src/main/java/edu/icet/repository/DocumentRepository.java
+++ src/main/java/edu/icet/repository/DocumentRepository.java
@@ -2,10 +2,14 @@ package edu.icet.repository;
 
 import edu.icet.dto.DocumentsDTO;
 
+import java.util.List;
+
 public interface DocumentRepository {
     void saveDocument(DocumentsDTO documentsDTO);
 
     boolean updateDocument(DocumentsDTO documentsDTO);
 
     boolean deleteById(String id);
+
+    List<DocumentsDTO> getAllDocument();
 }
--- src/main/java/edu/icet/repository/Impl/DocumentRepositoryImpl.java
+++ src/main/java/edu/icet/repository/Impl/DocumentRepositoryImpl.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class DocumentRepositoryImpl implements DocumentRepository {
@@ -40,5 +42,22 @@ public class DocumentRepositoryImpl implements DocumentRepository {
     public boolean deleteById(String id) {
         String sql = "DELETE FROM documents WHERE id=?";
         return jdbcTemplate.update(sql,id)>0;
+    }
+
+    @Override
+    public List<DocumentsDTO> getAllDocument() {
+        String sql = "SELECT * FROM documents";
+        List<DocumentsDTO> documentsDTOList = jdbcTemplate.query(sql, (rs, rowNum) -> {
+
+            DocumentsDTO documentsDTO = new DocumentsDTO();
+
+            documentsDTO.setId(rs.getLong(1));
+            documentsDTO.setUser_id(rs.getString(2));
+            documentsDTO.setFilea_name(rs.getString(3));
+            documentsDTO.setFile_path(rs.getString(4));
+            documentsDTO.setUploaded_at(rs.getTimestamp(5).toLocalDateTime());
+            return documentsDTO;
+        });
+        return documentsDTOList;
     }
 }
