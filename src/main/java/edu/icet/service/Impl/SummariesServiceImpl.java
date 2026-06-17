883c39420eaa072264efe9b59e834d075f064a2f
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-17 13:18:32 +0530

    Implement SummariesServiceImpl with saveSummarie method

---
+++ src/main/java/edu/icet/service/Impl/SummariesServiceImpl.java
@@ -0,0 +1,19 @@
+package edu.icet.service.Impl;
+
+import edu.icet.dto.SummariesDTO;
+import edu.icet.repository.SummariesRepository;
+import edu.icet.service.SummariesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SummariesServiceImpl implements SummariesService {
+
+    private final SummariesRepository summariesRepository;
+
+    @Override
+    public void saveSummarie(SummariesDTO summariesDTO) {
+        summariesRepository.saveSummarie(summariesDTO);
+    }
+}
