d3aefbab5928732d0846f88129cf65fb4b1a5e95
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-18 22:54:16 +0530

    Add updateSummary method to SummariesRepository interface

--- src/main/java/edu/icet/repository/SummariesRepository.java
+++ src/main/java/edu/icet/repository/SummariesRepository.java
@@ -4,4 +4,6 @@ import edu.icet.dto.SummariesDTO;
 
 public interface SummariesRepository {
     void saveSummary(SummariesDTO summariesDTO);
+
+    boolean updateSummary(SummariesDTO summariesDTO);
 }
