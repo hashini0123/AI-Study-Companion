aa3bb6bb0cc860dd2cd185cd7ef5e5bcfa78482e
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-18 22:50:56 +0530

    Refactor saveSummary method and add updateSummary endpoint in SummariesController

--- src/main/java/edu/icet/controller/SummariesController.java
+++ src/main/java/edu/icet/controller/SummariesController.java
@@ -3,11 +3,9 @@ package edu.icet.controller;
 import edu.icet.dto.SummariesDTO;
 import edu.icet.service.SummariesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +16,18 @@ public class SummariesController {
 
     @PostMapping("/save")
     public ResponseEntity<String> saveSummary(@RequestBody SummariesDTO summariesDTO){
-        summariesService.saveSummarie(summariesDTO);
+        summariesService.saveSummary(summariesDTO);
         return ResponseEntity.ok("Summary saved successfully!");
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> updateSummary(@RequestBody SummariesDTO summariesDTO){
+        boolean isUpdate = summariesService.updateSummary(summariesDTO);
+
+        if(isUpdate){
+            return ResponseEntity.ok("Summary update successfully!");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Summary not found!");
+        }
     }
 }
