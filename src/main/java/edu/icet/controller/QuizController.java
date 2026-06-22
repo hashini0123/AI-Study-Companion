d0a35d22e8637552be553d6d8f025367f7f5bbd2
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-22 23:45:36 +0530

    Add updateQuiz endpoint to QuizController for quiz updates

--- src/main/java/edu/icet/controller/QuizController.java
+++ src/main/java/edu/icet/controller/QuizController.java
@@ -3,11 +3,10 @@ package edu.icet.controller;
 import edu.icet.dto.QuizzesDTO;
 import edu.icet.service.QuizService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +19,17 @@ public class QuizController {
     public ResponseEntity<String> saveQuiz(@RequestBody QuizzesDTO quizzesDTO){
         quizService.saveQuiz(quizzesDTO);
         return ResponseEntity.ok("Document saved successfully!");
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> updateQuiz(@RequestBody QuizzesDTO quizzesDTO){
+        boolean isUpdate = quizService.updateQuiz(quizzesDTO);
+
+        if(isUpdate){
+            return ResponseEntity.ok("Quiz update successfully!");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Quiz not found!");
+        }
     }
 
 }
