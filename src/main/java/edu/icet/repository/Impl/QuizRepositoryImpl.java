3981b0aae9a697cde67b112559b159a96a36eaf8
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-22 16:49:22 +0530

    Add QuizRepositoryImpl for quiz data persistence implementation

---
+++ src/main/java/edu/icet/repository/Impl/QuizRepositoryImpl.java
@@ -0,0 +1,24 @@
+package edu.icet.repository.Impl;
+
+import edu.icet.dto.QuizzesDTO;
+import edu.icet.repository.QuizRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class QuizRepositoryImpl implements QuizRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void saveQuiz(QuizzesDTO quizzesDTO) {
+        String sql = "INSERT INTO quizzes (document_id, title, created_at) VALUES(?,?,?)";
+        jdbcTemplate.update(sql,
+                quizzesDTO.getDocument_id(),
+                quizzesDTO.getTitle(),
+                quizzesDTO.getCreated_at()
+        );
+    }
+}
