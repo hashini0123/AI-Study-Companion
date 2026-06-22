0e24cc214a73b9758aff6c6a8216512b4472c75c
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-22 12:47:41 +0530

    Add getAllSummary method to SummariesRepository for retrieving all summaries

--- src/main/java/edu/icet/repository/SummariesRepository.java
+++ src/main/java/edu/icet/repository/SummariesRepository.java
@@ -2,10 +2,14 @@ package edu.icet.repository;
 
 import edu.icet.dto.SummariesDTO;
 
+import java.util.List;
+
 public interface SummariesRepository {
     void saveSummary(SummariesDTO summariesDTO);
 
     boolean updateSummary(SummariesDTO summariesDTO);
 
     boolean deleteById(String id);
+
+    List<SummariesDTO> getAllSummary();
 }
