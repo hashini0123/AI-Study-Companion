31e747f48c4d2891b9d22f023f86074d93d3e414
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-22 12:47:04 +0530

    Add getAllSummary method to SummariesService for retrieving all summaries

--- src/main/java/edu/icet/service/SummariesService.java
+++ src/main/java/edu/icet/service/SummariesService.java
@@ -2,10 +2,14 @@ package edu.icet.service;
 
 import edu.icet.dto.SummariesDTO;
 
+import java.util.List;
+
 public interface SummariesService {
     void saveSummary(SummariesDTO summariesDTO);
 
     boolean updateSummary(SummariesDTO summariesDTO);
 
     boolean deleteSummary(String id);
+
+    List<SummariesDTO> getAllSummary();
 }
