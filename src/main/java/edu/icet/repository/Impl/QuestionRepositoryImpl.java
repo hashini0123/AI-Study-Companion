0f727afcbcc0a1631e1858d30e4943b11865fec5
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-24 22:31:15 +0530

    Implement getAllQuestion method in QuestionRepositoryImpl for retrieving all questions

--- src/main/java/edu/icet/repository/Impl/QuestionRepositoryImpl.java
+++ src/main/java/edu/icet/repository/Impl/QuestionRepositoryImpl.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class QuestionRepositoryImpl implements QuestionRepository {
@@ -45,5 +47,27 @@ public class QuestionRepositoryImpl implements QuestionRepository {
     public boolean deleteQuestion(String id) {
         String sql = "DELETE FROM questions WHERE id=?";
         return jdbcTemplate.update(sql, id)>0;
+    }
+
+    @Override
+    public List<QuestionsDTO> getAllQuestion() {
+        String sql = "SELECT * FROM questions";
+        List<QuestionsDTO> questionsDTOList = jdbcTemplate.query(sql, (rs, rowNum) -> {
+
+            QuestionsDTO questionsDTO = new QuestionsDTO();
+
+            questionsDTO.setId(rs.getLong(1));
+            questionsDTO.setQuiz_id(rs.getLong(2));
+            questionsDTO.setQuestion_text(rs.getString(3));
+            questionsDTO.setOption_a(rs.getString(4));
+            questionsDTO.setOption_b(rs.getString(5));
+            questionsDTO.setOption_c(rs.getString(6));
+            questionsDTO.setOption_d(rs.getString(7));
+            questionsDTO.setCorrect_option(rs.getString(8));
+            return questionsDTO;
+        });
+
+        return questionsDTOList;
+
     }
 }
