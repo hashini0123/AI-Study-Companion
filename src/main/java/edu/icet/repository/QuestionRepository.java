4b386837fe8e28342524231a369b4fd69458d806
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-24 22:31:03 +0530

    Add getAllQuestion method to QuestionRepository interface for retrieving all questions

--- src/main/java/edu/icet/repository/QuestionRepository.java
+++ src/main/java/edu/icet/repository/QuestionRepository.java
@@ -2,10 +2,14 @@ package edu.icet.repository;
 
 import edu.icet.dto.QuestionsDTO;
 
+import java.util.List;
+
 public interface QuestionRepository {
     void saveQuestion(QuestionsDTO questionsDTO);
 
     boolean updateQuestion(QuestionsDTO questionsDTO);
 
     boolean deleteQuestion(String id);
+
+    List<QuestionsDTO> getAllQuestion();
 }
