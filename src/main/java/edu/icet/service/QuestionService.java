a0eab20dfb2804e9007d01d508bd5e5d8d5fb6ac
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-24 22:30:38 +0530

    Add getAllQuestion method to QuestionService interface for retrieving all questions

--- src/main/java/edu/icet/service/QuestionService.java
+++ src/main/java/edu/icet/service/QuestionService.java
@@ -2,10 +2,14 @@ package edu.icet.service;
 
 import edu.icet.dto.QuestionsDTO;
 
+import java.util.List;
+
 public interface QuestionService {
     void saveQuestion(QuestionsDTO questionsDTO);
 
     boolean updateQuestion(QuestionsDTO questionsDTO);
 
     boolean deleteQuestion(String id);
+
+    List<QuestionsDTO> getAllQuestion();
 }
