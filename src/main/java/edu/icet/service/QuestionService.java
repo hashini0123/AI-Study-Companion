d7bb9cb38fe00f85998838f8119b0100298ed180
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-24 19:31:44 +0530

    Add updateQuestion method to QuestionService interface for updating questions

--- src/main/java/edu/icet/service/QuestionService.java
+++ src/main/java/edu/icet/service/QuestionService.java
@@ -4,4 +4,6 @@ import edu.icet.dto.QuestionsDTO;
 
 public interface QuestionService {
     void saveQuestion(QuestionsDTO questionsDTO);
+
+    boolean updateQuestion(QuestionsDTO questionsDTO);
 }
