f1b8d0199b527b000806cc7da000138a80092d3c
hashini0123 <nanayakkarahashini5@gmail.com>
2026-06-24 16:56:22 +0530

    Implement QuestionRepository interface in QuestionRepositoryImpl for saving questions

---
+++ src/main/java/edu/icet/repository/Impl/QuestionRepositoryImpl.java
@@ -0,0 +1,28 @@
+package edu.icet.repository.Impl;
+
+import edu.icet.dto.QuestionsDTO;
+import edu.icet.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class QuestionRepositoryImpl implements QuestionRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void saveQuestion(QuestionsDTO questionsDTO) {
+        String sql = "INSERT INTO questions (quiz_id, question_text, option_a, option_b, option_c, option_d, correct_option) VALUE (?,?,?,?,?,?,?) ";
+        jdbcTemplate.update(sql,
+                questionsDTO.getQuiz_id(),
+                questionsDTO.getQuestion_text(),
+                questionsDTO.getOption_a(),
+                questionsDTO.getOption_b(),
+                questionsDTO.getOption_c(),
+                questionsDTO.getOption_d(),
+                questionsDTO.getCorrect_option()
+        );
+    }
+}
